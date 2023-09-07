2032b9f58b26d2b3d00ca14771bda9d4d4bf48d3
godjupiters <godjupiters@gmail.com>
2023-09-07 14:09:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - this is the main put function
- * Return: always 0
+ * Return:0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle";
+puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
