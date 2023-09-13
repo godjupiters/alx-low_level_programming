7d40a1cc9129671da1ae4544d16aa597cd8e991b
godjupiters <godjupiters@gmail.com>
2023-09-13 15:44:51 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char z;
+
+	for (a = 0 ; a < 10; a++)
+	{
+		for (z = 'a' ; z <= 'z' ; z++)
+			_putchar(z);
+	}
+	_putchar('\n');
+}
