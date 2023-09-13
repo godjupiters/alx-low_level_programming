5bfe565e32d4403084792f624693ee80cae9ff36
godjupiters <godjupiters@gmail.com>
2023-09-13 13:52:19 +0100

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - task function
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
