8e206c8cd1a45f50f3c3095ca4b61fc65495b656
godjupiters <godjupiters@gmail.com>
2023-09-14 02:34:08 +0100

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+void times_table(void)
+{
+	int r, c, output;
+
+	for (r = 0 ; r < 10 ; r++)
+	{
+		for (c = 0 ; c < 10 ; c++)
+		{
+			output = r * c;
+			if (c == 0)
+				printf("%d, ", output);
+			else
+			{
+				printf("%2d", output);
+				if (r != 9)
+					printf(", ");
+			}
+
+		}
+	}
+}
