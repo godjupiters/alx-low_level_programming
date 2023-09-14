59cf14c46f1f13d75f0da0a166e018ab7264c5f0
godjupiters <godjupiters@gmail.com>
2023-09-14 15:08:55 +0100

    0-main.c, main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void positive_or_negative.(int a)
+{
+	if (a > 0)
+	{
+		printf("%d is positive\n", a);
+	}
+	else if (a < 0)
+	{
+		printf("%d is negative\n", a);
+	}
+	else
+	{
+		printf("%d is zero\n", a);
+	}
+}
