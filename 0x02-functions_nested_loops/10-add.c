c2cf03a853b9134911ffd8cd320380112f91ec4a
godjupiters <godjupiters@gmail.com>
2023-09-14 02:56:35 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - function adds two integers
+ * @y: first value
+ * @z: secong value
+ * Return: output of sum
+ */
+
+int add(int y, int z)
+{
+	return (y + z);
+}
