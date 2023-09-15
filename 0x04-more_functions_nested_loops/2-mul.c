45a4f4e1fa49d9680d8486cef83df13bbf8a5f3a
godjupiters <godjupiters@gmail.com>
2023-09-15 15:24:27 +0100

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
-/*
- * mul - prototype function
- * @a: holding variable 1
- * @b: holding variable 2
- * Return: result
+/**
+ * mul - Makes the sum of two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function.
+ * Return: The sum of the two parameters
  */
 
 int mul(int a, int b)
