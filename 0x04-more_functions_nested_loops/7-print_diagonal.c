c42fb67421f780daaafe30239beb35e702edb2de
godjupiters <godjupiters@gmail.com>
2023-09-15 23:24:19 +0100

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,23 +9,17 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		int a, b;
 
 		for (a = 0 ; a < n ; a++)
-		{
+
 			for (b = 0 ; b <= a ; b++)
-			{
 				_putchar(' ');
-			}
-			{
-				_putchar('\\');
-				_putchar('\n');
-			}
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 }
