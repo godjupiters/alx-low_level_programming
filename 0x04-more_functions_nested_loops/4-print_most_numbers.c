c577fb56bf70fa98251651fe30f8ea464ee13c6f
godjupiters <godjupiters@gmail.com>
2023-09-15 16:59:10 +0100

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function print <= 9, != 2 && 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int v;
+
+	for ((v = 0 ; v < 10 ; v++) && (v != 2 ; v != 4))
+	{
+		_putchar(v + '0');
+	}
+		_putchar('\n');
+}
