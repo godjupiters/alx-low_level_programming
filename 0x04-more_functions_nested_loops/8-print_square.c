64e27363ef5e12c45ce844603fdae01e0b504ca1
godjupiters <godjupiters@gmail.com>
2023-09-15 23:51:46 +0100

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints squares with hashtag
+ * @size: variable holding the size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+		for (a = 1 ; a <= size a++)
+		{
+			_putchar('#');
+		}
+		{
+			_putchar('\n');
+		}
+	}
+}
+
+
+
+
