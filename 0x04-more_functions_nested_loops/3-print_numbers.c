7bfead3b425e2eb516c36192b95d6e586babf47c
godjupiters <godjupiters@gmail.com>
2023-09-15 16:31:01 +0100

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - call function to check number <= 9
+ *
+ * Description: a simple call function
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int v;
+
+	for (v = 0 ; v <= 9 ; v++)
+	{
+		_putchar(v);
+	}
+	_putchar('\n');
+}
