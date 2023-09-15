441f5c1388edb98947684d771480fe4d34793974
godjupiters <godjupiters@gmail.com>
2023-09-15 18:37:40 +0100

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints an underscore
+ * @n: variable holding the no. of times
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+	else
+	{
+		int m;
+
+		for (m = 0 ; m < n ; m++)
+		{
+			_putchar('_');
+		}
+			_putchar('\n');
+	}
+}
