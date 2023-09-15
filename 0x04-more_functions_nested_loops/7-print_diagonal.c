d6efae46ad77527a6988277df58e10aa1d60d357
godjupiters <godjupiters@gmail.com>
2023-09-15 19:05:11 +0100

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print forwardslash
+ * @n: number of forwardslash to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0 ; a < n ; a++)
+		{
+			for (b = 0 ; b <= a ; b++)
+			{
+				_putchar(' ');
+			}
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+		}
+	}
+}
