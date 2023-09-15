e097ad4f170e43f1314e6597a875f505c622e273
godjupiters <godjupiters@gmail.com>
2023-09-15 23:55:49 +0100

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,23 +9,13 @@
 void print_square(int size)
 {
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		int a, b;
 
 		for (a = 1 ; a <= size a++)
-		{
 			_putchar('#');
-		}
-		{
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
-
-
-
-
