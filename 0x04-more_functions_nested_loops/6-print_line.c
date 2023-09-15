63866361a5fdfad160cdc179f12eb0bf1dacde2d
godjupiters <godjupiters@gmail.com>
2023-09-15 18:47:24 +0100

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,13 +9,17 @@
 void print_line(int n)
 {
 	if (n <= 0)
-		_putchar('\n')
+	{
+		_putchar('\n');
+	}
 	else
 	{
 		int m;
 
-		for (m = 1 ; m <= n ; m++)
+		for (m = 0 ; m < n ; m++)
+		{
 			_putchar('_');
-		_putchar('\n');
+		}
+			_putchar('\n');
 	}
 }
