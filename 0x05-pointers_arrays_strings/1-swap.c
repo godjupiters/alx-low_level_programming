324c20573cd124fdfde4dd3af66d9de05ed1185e
godjupiters <godjupiters@gmail.com>
2023-09-18 15:45:21 +0100

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+ * swap_int(int *a, int *b) - a prototype function that swaps
+ * the values of two integers
+ * @a: pointer variable 1
+ * @b: pointer variable 2
+ * Return: a, b
+ */
+
+void swap_int(int *a, int *b);
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
