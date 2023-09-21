34d488d5351305d93e31c122f673274162d1cfff
godjupiters <godjupiters@gmail.com>
2023-09-21 22:11:41 +0100

    7-leet.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -6,14 +6,12 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
-{
-    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-    char *p;
+{	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+	char *p;
 
-    p = leet(s);
-    printf("%s", p);
-    printf("%s", s);
-    return (0);
+	p = leet(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
 }
