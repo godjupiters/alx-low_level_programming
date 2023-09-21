1ac69f6ef19991bae2d0b97a7d77a3cf8148d8cf
godjupiters <godjupiters@gmail.com>
2023-09-21 17:22:31 +0100

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * cap_string - prototype function that capitalizes all words of a string.
+ * Separators of words: space, tabulation,
+ * new line, ,, ;, ., !, ?,
+ * 
+ * @x: pointer variable
+ *
+ * Return: x
+ */
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * leet -  prototype function that encodes a string into 1337.
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ *
+ * @x: pointer variable
+ * Return: x
+ */
+
+char *leet(char *x)
+{
+	int a, b;
+	char v1[] = "aAeEoOtTlL";
+	char v2[] = "4433007711";
+
+	for (a = 0; x[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (x[a] == v1[b])
+			{
+				x[a] = v2[b];
+			}
+		}
+	}
+
+	return (x);
+}
+
