6594a92428a7f2f370235bba8e72f1f60c680746
godjupiters <godjupiters@gmail.com>
2023-09-21 13:45:52 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
 /**
- * strncat - prototype function that concatenates two strings.
+ * _strncat - prototype function that concatenates two strings.
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated
+ * if it contains n or more bytes
+ *
  * @dest: variable for input 1
  * @src: variable for input 2
  * @n: variable interger for input 3
- * Return: dest value
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
