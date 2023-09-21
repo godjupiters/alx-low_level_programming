51d16ab94fad2c6539c984ae90222410cd584761
godjupiters <godjupiters@gmail.com>
2023-09-21 13:34:45 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncat - prototype function that concatenates two strings.
+ * @dest: variable for input 1
+ * @src: variable for input 2
+ * @n: variable interger for input 3
+ */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+	dest[a] = src[b];
+	a++;
+	b++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
+
