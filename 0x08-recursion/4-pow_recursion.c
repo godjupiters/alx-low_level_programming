e715e29548c39ad38df3aab08662d5d1c7e2ee7c
godjupiters <godjupiters@gmail.com>
2023-09-28 18:07:25 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a prototype function that returns the value of x
+ * raised to the power of y
+ * @x: variable to be raised
+ * @y: variablle holding the power
+ * Return: result empowered
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
