c69093675d0658885a2a8f3496eb8aefd6d6b5b8
godjupiters <godjupiters@gmail.com>
2023-10-20 17:52:22 +0100

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - a function for adding new node to linked list
+ * @head: a variable holding the double pointer
+ * @str: a variable holding the string for new node
+ * Return: address or NULL
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		{
+			return (new);
+		}
+	}
+
+
+	while (temp->next)
+		temp = temp->next;
+
+
+	temp->next = new;
+	{
+	return (new);
+	}
+}
