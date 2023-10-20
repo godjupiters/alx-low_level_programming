099144136465431fb64d932f73abc705eb363554
godjupiters <godjupiters@gmail.com>
2023-10-20 22:48:01 +0100

    print

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,13 +2,13 @@
     extern printf
 
     section .data
-        hello: db "Hello, Holberton", 10, 0 ; define a string with a newline
+        msg: db "Hello, Holberton", 10, 0
 
     section .text
         main:
-            mov rbp, rsp ; set up the stack frame
-            lea rdi, [rel hello] ; load the address of the string
-            mov rax, 0 ; set the number of floating point arguments to zero
-            call printf ; invoke the printf function
-            mov rsp, rbp ; restore the stack pointer
-            ret ; return from main
+            push rbp
+            mov rdi, msg
+            xor rax, rax
+            call printf
+            pop rbp
+            ret
