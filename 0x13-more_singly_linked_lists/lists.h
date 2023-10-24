6848eefa9dc248edb2466549e2829385237929cf
godjupiters <godjupiters@gmail.com>
2023-10-24 12:15:10 +0100

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,9 +8,7 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * 
  */
 
 typedef struct listint_s
@@ -18,10 +16,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
-/**
- * Prototype Functions
- */
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
@@ -35,8 +29,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-
-
 
 
 #endif
